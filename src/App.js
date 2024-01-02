906f2ffa6746d10b1e8e940ac560196ed839cd02
dominhdang148 <myclone235@gmail.com>
2024-01-02 22:39:20 +0700

    finished lesson 5

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './NavBar';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 40;
-  // const person = {
-  //   name: "dang",
-  //   age: 21,
-  // }
-  const link = "http://www.google.com"
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked {likes} time</p>
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{"Hello"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
-    </div>
+    </div >
   );
 }
 
