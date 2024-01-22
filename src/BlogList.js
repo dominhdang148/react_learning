bbdea72381c417830c53520c30c4144d2c6c7930
dominhdang148 <myclone235@gmail.com>
2024-01-22 22:07:20 +0700

    Finished lesson 13

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,6 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
     // const blogs = props.blogs;
     // const title = props.title;
-
-
 
 
     return (
@@ -12,6 +10,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
